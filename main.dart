576bde2a9bc7bf488b77b16014d6f73fe25ebe2f
mhmmdrafif18 <rafifmuzakki1007@gmail.com>
2026-02-26 11:07:19 +0700

    Mengubah main.dart untuk coba kalkulator

--- main.dart
+++ main.dart
@@ -1,3 +1,13 @@
+import 'dart:io';
+import 'kalkulator.dart';
+
 void main() {
-  print("Hello Dart Super CLI");
+  stdout.write("Masukkan angka pertama: ");
+  int a = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Masukkan angka kedua: ");
+  int b = int.parse(stdin.readLineSync()!);
+
+  int hasil = hitungTambah(a, b);
+  print("Hasil penjumlahan: $hasil");
 }
